87bec10c18bf8a0baa4edd77bc25a32b1ff79a32
Timon Borter <timon.borter@gmx.ch>
2020-03-20 08:51:52 +0100

    configure travis-ci integration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.2.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+
+    id("org.sonarqube") version "2.7.1"
+
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
 }
 
 group = "io.github.bbortt.buessle.app"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "bbortt_buessle_app")
+    }
+}
 
 allprojects {
     version = "${version}"
