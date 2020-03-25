9509960e95388fe005f090e74915fa1acf6b5e5d
Timon Borter <timon.borter@gmx.ch>
2020-03-25 16:31:58 +0100

    remove logging aspect configuration. annotate component on class.

--- src/main/kotlin/io/github/bbortt/buessle/app/aspect/ApiLoggingAspect.kt
+++ src/main/kotlin/io/github/bbortt/buessle/app/aspect/ApiLoggingAspect.kt
@@ -7,8 +7,10 @@ import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 
 @Aspect
+@Component
 class ApiLoggingAspect {
 
     companion object {
