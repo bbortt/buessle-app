09497dfab02d70a0e27aeebd79221654346bb60c
Timon Borter <timon.borter@gmx.ch>
2020-03-28 13:59:33 +0100

    handle socket errors too. no close for the moment.

--- src/main/webapp/src/redux/socket/index.js
+++ src/main/webapp/src/redux/socket/index.js
@@ -1,35 +1,14 @@
 // @flow
-import getStore from '../getStore'
-
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
+const { socketUrl } = publicRuntimeConfig
 
-export class Socket {
-  +socket: WebSocket
+let socket: WebSocket
 
-  constructor() {
-    const { socketUrl } = publicRuntimeConfig
-    this.socket = new WebSocket(`${socketUrl}/sockets`)
-  }
-
-  open = () => {
-    this.socket.onmessage = (event: MessageEvent) => {
-      const { topic, payload } = (event.data: any)
-      getStore().dispatch({ type: topic, payload })
-    }
-  }
-
-  close = () => {
-    this.socket.close()
-  }
-}
-
-let socket: Socket
-
-export const getSocket = (): Socket => {
+export const getSocket = (): WebSocket => {
   if (!socket) {
-    socket = new Socket()
+    socket = new WebSocket(`${socketUrl}/sockets`)
   }
 
   return socket
