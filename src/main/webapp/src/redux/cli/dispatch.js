796dd5c7d2c793475069a01ed94d879b07e52083
Timon Borter <timon.borter@gmx.ch>
2020-05-01 10:18:47 +0200

    bugfixing action via cli dispatcher.

--- src/main/webapp/src/redux/cli/dispatch.js
+++ src/main/webapp/src/redux/cli/dispatch.js
@@ -2,10 +2,15 @@
 import type { Action } from 'redux'
 import getStore from '../getStore'
 
-import { AVAILABLE_REDUX_ACTIONS } from './listActions'
+import { ADD_PLAYER } from '../action'
+
+const AVAILABLE_REDUX_ACTION_TYPES = [ADD_PLAYER]
 
 export const dispatch = (action: Action) => {
-  if (!action.type || AVAILABLE_REDUX_ACTIONS.indexOf(action.type) === -1) {
+  if (
+    !action.type ||
+    AVAILABLE_REDUX_ACTION_TYPES.indexOf(action.type) === -1
+  ) {
     throw 'This action type is not dispatchable!'
   }
 
